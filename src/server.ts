bcfdc6ed1c858e31c34bbfec1f4d3bfd94c9b8b9
Ayrton Santos <ayrton.arc@gmail.com>
2023-05-26 17:07:30 -0700

    server workong

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,23 @@
-console.log("API ")
+import express from 'express';
+import cors from 'cors';
+import compression from "compression";
+import { createServer } from 'http';
+
+
+const app = express();
+
+app.use(cors());
+
+app.use(compression())
+
+app.get('/', (req, res) => {
+     res.send('API')
+  });
+
+const httpServer = createServer(app);
+
+httpServer.listen({
+    port: 2002
+},
+() => console.log('http://localhost:2002')
+);
