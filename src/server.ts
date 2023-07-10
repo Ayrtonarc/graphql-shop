98347ca21f59c63583369b29ac6bdfb9e826d5b9
Ayrton Santos <ayrton.arc@gmail.com>
2023-07-10 16:53:18 -0700

    81 variables de entorno

--- src/server.ts
+++ src/server.ts
@@ -2,22 +2,33 @@ import express from 'express';
 import cors from 'cors';
 import compression from "compression";
 import { createServer } from 'http';
+import enviroments from './config/enviroments';
+//configuracion de las variables de entorno (lectura)
+if(process.env.NODE_ENV !== 'production'){
+  const env = enviroments;
+  console.log(env);
+}
 
 
-const app = express();
 
-app.use(cors());
+async function init() {
+  const app = express();
 
-app.use(compression())
+  app.use(cors());
 
-app.get('/', (req, res) => {
-     res.send('API')
+  app.use(compression())
+
+  app.get('/', (req, res) => {
+    res.send('API')
   });
+  const PORT = process.env.PORT || 2002
+  const httpServer = createServer(app);
 
-const httpServer = createServer(app);
+  httpServer.listen({
+    port: PORT
+  },
+    () => console.log(`http://localhost:${PORT}`)
+  );
 
-httpServer.listen({
-    port: 2002
-},
-() => console.log('http://localhost:2002')
-);
+}
+init();
