3650dafe5b00f7c04200672e7da7d314aab34f28
Ayrton Santos <ayrton.arc@gmail.com>
2023-07-10 23:18:43 -0700

    86 configurar apollo server express y users query

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,9 @@ import cors from 'cors';
 import compression from "compression";
 import { createServer } from 'http';
 import enviroments from './config/enviroments';
+import { ApolloServer } from 'apollo-server-express';
+import schema from './schema';
+import expressPlayground from 'graphql-playground-middleware-express';
 //configuracion de las variables de entorno (lectura)
 if(process.env.NODE_ENV !== 'production'){
   const env = enviroments;
@@ -17,10 +20,17 @@ async function init() {
   app.use(cors());
 
   app.use(compression())
-
-  app.get('/', (req, res) => {
-    res.send('API')
+  
+  const server = new ApolloServer({
+      schema,
+      introspection: true
   });
+server.applyMiddleware({app});
+
+  app.get('/', expressPlayground({
+    endpoint: '/graphql'
+  }));
+  
   const PORT = process.env.PORT || 2002
   const httpServer = createServer(app);
 
