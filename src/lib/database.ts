02114673b25ca3da906bdbeb374a48a9ffbe20ae
Ayrton Santos <ayrton.arc@gmail.com>
2023-07-11 22:32:16 -0700

    89 Conexion a base de datos

---
+++ src/lib/database.ts
@@ -0,0 +1,25 @@
+import chalk from 'chalk';
+import  MongoClient  from 'mongodb';
+
+class Database {
+    async init (){
+        const MONGO_DB = process.env.DATABASE || 'mongodb://localhost:27017/meang-online-shop';
+        const client = await MongoClient.MongoClient.connect(
+            MONGO_DB,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            }
+        );
+
+        const db = client.db();
+
+        if(client.isConnected()){
+            console.log('===DB===');
+            console.log(`STATUS ${chalk.greenBright('ONLINE')}`);
+            console.log(`STATUS ${chalk.greenBright(db.databaseName)}`);
+        }
+    }
+}
+
+export default Database;
