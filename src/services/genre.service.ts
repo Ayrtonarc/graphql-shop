f7f55a327195b90259332a7f07312bc93096ba9b
Ayrton Santos <ayrton.arc@gmail.com>
2024-07-09 15:13:07 -0700

    194 servicio de operaciones de los Resolvers

---
+++ src/services/genre.service.ts
@@ -0,0 +1,18 @@
+class GenreService extends ResolversOperationsService {
+    constructor(root: object, variables: object, context: object) {
+       super(root, variables, context);
+
+    }
+
+    //Listar informacion
+    protected list(collection: string, listElement: string) {
+        
+    }
+    //Obtener Detalles del item
+
+    //Anadir Item
+
+    // Modificar el item 
+
+    //Eliminar item
+}
---
+++ src/services/resolvers-operations.service.ts
@@ -0,0 +1,21 @@
+class ResolversOperationsService {
+    private root: object;
+    private variables: object;
+    private context: object;
+    constructor(root: object, variables: object, context: object) {
+       this.root = root;
+       this.variables = variables;
+       this.context = context;
+    }
+
+    //Listar informacion
+    protected list(collection: string, listElement: string) {
+    }
+    //Obtener Detalles del item
+
+    //Anadir Item
+
+    // Modificar el item 
+
+    //Eliminar item
+}
