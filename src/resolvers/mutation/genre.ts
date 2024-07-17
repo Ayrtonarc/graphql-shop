927ea3bc46aabd9855cbc9ac507d00b41c3e22ab
Ayrton Santos <ayrton.arc@gmail.com>
2024-07-16 21:09:14 -0700

    199 Anadir genero Comenzar definiendo el Schema y anadir el resolver

---
+++ src/resolvers/mutation/genre.ts
@@ -0,0 +1,13 @@
+import { IResolvers } from "graphql-tools";
+import GenreService from "../../services/genre.service";
+
+const resolversGenreMutation: IResolvers = {
+    Mutation: {
+        addGenre(_, variables, context) {
+            //anadimos llamada al servicio
+            return new GenreService(_, variables, context).items();
+        }
+    }
+};
+
+export default resolversGenreMutation;
